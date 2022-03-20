8f9505728ac56eaf96a53300200b0aeb656f0d6c
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-20 18:04:45 +0000

    Update index.java

--- index.java
+++ index.java
@@ -120,6 +120,33 @@ public class TicketMachine
         balance = 0;
     }
 }
+
+
+public class TicketMachine
+{
+    // The price of a ticket from this machine.
+    private int price;
+    // The amount of money entered by a customer so far.
+    private int balance;
+    // The total amount of money collected by this machine.
+    private int total;
+    //The price is given to customers
+    
+    /**
+     * Create a machine that issues tickets of the given price.
+     * Note that the price must be greater than zero, and there
+     * are no checks to ensure this.
+     */
+    public TicketMachine(int cost)
+    {
+        price = cost;
+        balance = 0;
+        total = 0;      
+        
+    }
+
+ 
+}/**
   /**
  * TicketMachine models a naive ticket machine that issues
  * flat-fare tickets.
