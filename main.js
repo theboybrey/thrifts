f65def0e5b77c416654c954751a1cf177157d68b
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-24 15:13:08 +0000

    Update main.js

--- main.js
+++ main.js
@@ -15,6 +15,61 @@ const generateColor = () => {
     hex.innerHTML = `#` + randomColor; 
 
 }
+const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate');
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+
+    document.body.style.backgroundColor = `#` + randomColor;
+    hex.innerHTML = `#` + randomColor; 
+
+}const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate');
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+
+    document.body.style.backgroundColor = `#` + randomColor;
+    hex.innerHTML = `#` + randomColor; 
+
+}const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate');
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+
+    document.body.style.backgroundColor = `#` + randomColor;
+    hex.innerHTML = `#` + randomColor; 
+
+}const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate');
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+
+    document.body.style.backgroundColor = `#` + randomColor;
+    hex.innerHTML = `#` + randomColor; 
+
+}const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate');
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+
+    document.body.style.backgroundColor = `#` + randomColor;
+    hex.innerHTML = `#` + randomColor; 
+
+}const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate');
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+
+    document.body.style.backgroundColor = `#` + randomColor;
+    hex.innerHTML = `#` + randomColor; 
+
+}
 
 btn.addEventListener("click", generateColor);
 generateColor();
