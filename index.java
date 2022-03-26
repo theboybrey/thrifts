d792187de39f386090eb1ad4b7d41c5d6b5bbff3
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-27 05:39:07 +0800

    Update index.java

--- index.java
+++ index.java
@@ -34,6 +34,28 @@ public class TicketMachine
         
     }
 
+  public class TicketMachine
+{
+    // The price of a ticket from this machine.
+    private int price;
+    // The amount of money entered by a customer so far.
+    private int balance;
+    // The total amount of money collected by this machine.
+    private int total;
+    //The price is given to customers
+    private int cusPrice;
+    /**
+     * Create a machine that issues tickets of the given price.
+     * Note that the price must be greater than zero, and there
+     * are no checks to ensure this.
+     */
+    public TicketMachine(int cost)
+    {
+        price = cost;
+        balance = 0;
+        total = 0;      
+        
+    }
     /**
      * Return the price of a ticket.
      */
