5ae5265df489c4a5adde41f0d3f25b8a23915451
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-26 21:47:35 +0000

    Update index.java

--- index.java
+++ index.java
@@ -33,6 +33,35 @@ public class TicketMachine
         total = 0;      
         
     }
+  {
+    // The price of a ticket from this machine.
+    private int price;
+    // The amount of money entered by a customer so far.
+    private int balance;
+    // The total amount of money collected by this machine.
+    private int total;
+    //The price is given to customers
+    private int cusPrice;
+    /**
+     * Create a machine that issues tickets of the given price.
+     * Note that the price must be greater than zero, and there
+     * are no checks to ensure this.
+     */
+    public TicketMachine(int cost)
+    {
+        price = cost;
+        balance = 0;
+        total = 0;      
+        
+    }
+    
+    /**Adding an Accessor method that gets the Total
+     * 
+    */
+    public int getTotal(){
+         return total;
+    }
+    
 
   public class TicketMachine
 {
