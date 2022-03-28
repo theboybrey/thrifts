b34c0ef3ce10c555ebf649a576f594576b9e1e94
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-28 08:22:40 +0000

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -25,6 +25,32 @@ const toggleDarkMode = document.querySelector(".toggle-darkmode");
 const toggleText = document.querySelector(".toggle-text");
 
 let darkMode = localStorage.getItem("darkMode");
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
+
+// Set Dark Mode
+const enableDarkMode = () => {
+  document.body.classList.add("darkmode");
+  toggleText.textContent = "Light";
+  localStorage.setItem("darkMode", "enabled");
+};
+const toggleDarkMode = document.querySelector(".toggle-darkmode");
+const toggleText = document.querySelector(".toggle-text");
+
+let darkMode = localStorage.getItem("darkMode");
 
 const toggleDarkMode = document.querySelector(".toggle-darkmode");
 const toggleText = document.querySelector(".toggle-text");
